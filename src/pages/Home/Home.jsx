7a53f34e2178fa4cbf750876e4e00ac908bef06f
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-11-15 00:31:13 +0600

    Rooms section aadd

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Carousel2 from '../../components/Home/Carousel';
+import ExplorePlaces from '../../components/Home/Rooms';
 
 const Home = () => {
     return (
         <div className=''>
-            <div className="">
+            <div className="container -mt-4">
                 <Carousel2 />
+                <ExplorePlaces/>
             </div>
         </div>
     );
